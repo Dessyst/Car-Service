9bcc538b7e37017ad25a4cd7101225bf513e9d48
Desislava Stefanova <dessy.stephanova@gmail.com>
2023-05-16 23:35:07 +0300

    test3

--- src/main/java/com/example/demo/repository/OrderRepository.java
+++ src/main/java/com/example/demo/repository/OrderRepository.java
@@ -1,8 +1,8 @@
 package com.example.demo.repository;
 
-
-import com.ssan.demo.entity.Order;
+import com.example.demo.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 public interface OrderRepository {
 }
--- src/main/java/com/example/demo/repository/ServiceRepository.java
+++ src/main/java/com/example/demo/repository/ServiceRepository.java
@@ -1,4 +1,8 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 public interface ServiceRepository {
 }
